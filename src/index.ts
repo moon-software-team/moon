44c84f5a69f1b562ba3380f5f312c243fc443a9a
mallory-scotton <mallory.scotton@epitech.eu>
2025-07-29 17:28:41 +0200

    add: adding tests file

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,66 @@
 import './app';
 import { app, BrowserWindow } from 'electron';
 
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
+// if (require('electron-squirrel-startup')) {
+//   app.quit();
+// }
 
-process.moon.serve();
+// process.moon.serve();
 
-app.on('ready', process.moon.create);
+// app.on('ready', process.moon.create);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    process.moon.create();
-  }
-});
+// app.on('activate', () => {
+//   if (BrowserWindow.getAllWindows().length === 0) {
+//     process.moon.create();
+//   }
+// });
 
-// import { VLC } from './utils';
+import { VLC } from './utils';
 
-// const sleep = async (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = async (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// const vlc = new VLC();
+const vlc = new VLC();
 
-// (async () => {
-//   await vlc.open(
-//     [
-//       '--fullscreen',
-//       '--no-video-title-show',
-//       '--no-osd',
-//       '--no-random',
-//       '--no-loop',
-//       '--no-repeat',
-//       '--video-on-top',
-//       '--no-embedded-video',
-//       '--qt-minimal-view',
-//       '--qt-notification=0',
-//       '--extraintf=dummy'
-//     ],
-//     '/home/mallow/Téléchargements/Elemental.2023.MULTI.VFF.1080p.WEBRip.7.1x265-Elcrackito/Elemental.2023.MULTI.VFF.1080p.WEBRip.7.1x265-Elcrackito.mkv'
-//   );
+(async () => {
+  await vlc.open(
+    [
+      '--fullscreen',
+      '--no-video-title-show',
+      '--no-osd',
+      '--no-random',
+      '--no-loop',
+      '--no-repeat',
+      '--video-on-top',
+      '--no-embedded-video',
+      '--qt-minimal-view',
+      '--qt-notification=0',
+      '--extraintf=dummy'
+    ],
+    process.argv[2]
+  );
 
-//   const duration = await vlc.getLength();
-//   console.log('Duration:', duration);
+  const duration = await vlc.getLength();
+  console.log('Duration:', duration);
 
-//   const atracks = await vlc.getAudioTracks();
-//   console.log('Audio Tracks:', atracks);
+  const atracks = await vlc.getAudioTracks();
+  console.log('Audio Tracks:', atracks);
 
-//   await vlc.seek(500);
+  await vlc.seek(500);
 
-//   await sleep(2000);
+  await sleep(2000);
 
-//   await vlc.setAudioTrack(1);
+  await vlc.setAudioTrack(1);
 
-//   const stracks = await vlc.getSubtitleTracks();
-//   console.log('Subtitle Tracks:', stracks);
+  const stracks = await vlc.getSubtitleTracks();
+  console.log('Subtitle Tracks:', stracks);
 
-//   await sleep(10000);
+  await sleep(10000);
 
-//   await vlc.stop();
-// })();
+  await vlc.stop();
+})();
